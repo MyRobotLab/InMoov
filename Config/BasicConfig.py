c34b81d8054c61a8d9c91613516ce56b3744fc0c
Anthony <moz4r@free.fr>
2016-12-11 00:49:02 +0100

    Gaël work from inmoov folder
    + new folder AIML

---
+++ Config/BasicConfig.py
@@ -0,0 +1,5 @@
+TODO:
+#set here the minimum config so that InMoov can work
+#a finger starter or a full inmoov should be able to work with this
+#min and max?
+#mappings?
---
+++ Config/ExtraConfig/Import_Libraries.py
@@ -0,0 +1,20 @@
+from java.lang import String
+from org.myrobotlab.net import BareBonesBrowserLaunch
+from datetime import datetime
+from subprocess import Popen, PIPE
+#######################
+import threading
+import time
+import random
+import urllib, urllib2
+import json
+import io
+import itertools
+import textwrap
+import codecs
+import socket
+import os
+import shutil
+import hashlib
+import subprocess
+import csv
