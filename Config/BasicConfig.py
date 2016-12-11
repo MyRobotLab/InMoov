863cbf2951b456be6fa90e5af1fd745b382b4b79
Anthony <moz4r@free.fr>
2016-12-11 13:31:16 +0100

    [fingerstarter] start to work on basic config

--- Config/BasicConfig.py
+++ Config/BasicConfig.py
@@ -1,5 +1,21 @@
-TODO:
+#TODO:
 #set here the minimum config so that InMoov can work
 #a finger starter or a full inmoov should be able to work with this
 #min and max?
 #mappings?
+
+# FingerStarter: connect one arduino and play with servo
+# NoArduino: Chatbot Only
+# Full: Inmoov is Alive
+ScriptType="FingerStarter" 
+
+
+# ##############################################################################
+# BASIC CONFIGURATION
+# 
+# ##############################################################################
+
+
+MyLanguage="EN" # LANGUAGE ( EN / FR / todo... )
+MyRightPort="COM7" # Your Arduino ComPort : UPLOAD MRLCOMM INSIDE !!!
+
