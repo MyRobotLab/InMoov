600f73394fc7d19de98edee8da125d9400a67eba
Langevin Gael <hairygael@gmail.com>
2017-01-01 19:35:44 +0100

    Update AutoListen.py

--- InmoovScript/inmoovLife/AutoListen.py
+++ InmoovScript/inmoovLife/AutoListen.py
@@ -6,17 +6,17 @@
 # Timer function to autostart webkit microphone every 10seconds
 # only if robot not actualy speaking
 ###############################################################################
-WebkitSpeachReconitionFix = Runtime.start("WebkitSpeachReconitionFix","Clock")
-WebkitSpeachReconitionFix.setInterval(15000)
+WebkitSpeechRecognitionFix = Runtime.start("WebkitSpeechRecognitionFix","Clock")
+WebkitSpeechRecognitionFix.setInterval(15000)
 
-def WebkitSpeachReconitionON(timedata):
+def WebkitSpeechRecognitionON(timedata):
 
 	if RobotIsActualySpeaking==0:
 		ear.resumeListening()
 	
 
-WebkitSpeachReconitionFix.addListener("pulse", python.name, "WebkitSpeachReconitionON")
+WebkitSpeechRecognitionFix.addListener("pulse", python.name, "WebkitSpeechRecognitionON")
 
-WebkitSpeachReconitionFix.startClock()
+WebkitSpeechRecognitionFix.startClock()
 
 
