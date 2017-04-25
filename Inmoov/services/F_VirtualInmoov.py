b9ee8f20bfc65845140b092595839354d9e34eb1
Anthony <moz4r@free.fr>
2017-04-26 00:47:02 +0200

    fix wainting the jar

--- Inmoov/services/F_VirtualInmoov.py
+++ Inmoov/services/F_VirtualInmoov.py
@@ -5,7 +5,7 @@
 if ScriptType=="Virtual":talkEvent(lang_startingVirtual)
 
 #virtual inmoov
-i01.VinmoovFullScreen=0
-i01.VinmoovBackGroundColor="Grey"
-i01.VinmoovWidth=800
-i01.VinmoovHeight=600
+#i01.VinmoovFullScreen=0
+#i01.VinmoovBackGroundColor="Grey"
+#i01.VinmoovWidth=800
+#i01.VinmoovHeight=600
