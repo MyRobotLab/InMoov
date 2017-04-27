f5854ebd16d96de3bc3314dc07e9c712b911bbbc
Langevin Gael <hairygael@gmail.com>
2017-04-27 12:45:28 +0200

    Update trackPoint.py

--- Inmoov/inmoovGestures/COMPLETE_GESTURES/trackPoint.py
+++ Inmoov/inmoovGestures/COMPLETE_GESTURES/trackPoint.py
@@ -1,5 +1,8 @@
 def trackPoint():
+  global RobotCanMoveHeadWhileSpeaking
+  RobotCanMoveHeadWhileSpeaking=0
   i01.headTracking.startLKTracking()
   i01.eyesTracking.startLKTracking()
+  i01.setHeadVelocity(80, -1)
+  sleep(1)
   fullspeed()
-
