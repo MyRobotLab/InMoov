588f2f2d3f21c7010bc192383c179a4bed066f57
Langevin Gael <hairygael@gmail.com>
2017-04-27 19:28:23 +0200

    Update fullspeed.py

--- Inmoov/inmoovGestures/COMPLETE_GESTURES/fullspeed.py
+++ Inmoov/inmoovGestures/COMPLETE_GESTURES/fullspeed.py
@@ -1,7 +1,7 @@
 def fullspeed():
-  #neopixel.setAnimation("Color Wipe", 200, 0, 0, 1)
-  #sleep(1)
-  #neopixel.setAnimation("Ironman", 0, 0, 255, 1)
+  neopixel.setAnimation("Color Wipe", 200, 0, 0, 1)
+  sleep(1)
+  neopixel.setAnimation("Ironman", 0, 0, 255, 1)
   i01.setHandSpeed("left", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
   i01.setHandSpeed("right", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
   i01.setArmSpeed("left", 1.0, 1.0, 1.0, 1.0)
