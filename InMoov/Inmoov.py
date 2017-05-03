fc32cc5c8be6d0edacf818acd5c3d8862cb5009b
Anthony <moz4r@free.fr>
2017-05-03 21:00:19 +0200

    start webgui after ear
    https://github.com/MyRobotLab/myrobotlab/issues/62

--- InMoov/Inmoov.py
+++ InMoov/Inmoov.py
@@ -24,16 +24,6 @@ mrlCompatible='2066'
 
 
 
-# Start the webgui service without starting the browser
-webgui = Runtime.create("WebGui","WebGui")
-webgui.autoStartBrowser(False)
-webgui.startService()
-# Then start the browsers and show the WebkitSpeechRecognition service named i01.ear
-webgui.startBrowser("http://localhost:8888/#/service/i01.ear")
-# As an alternative you can use the line below to show all services in the browser. In that case you should comment out all lines above that starts with webgui. 
-# webgui = Runtime.createAndStart("webgui","WebGui")
-
-
 ##############
 # Main inmoov service declaration
 i01 = Runtime.createAndStart("i01", "InMoov")
--- InMoov/services/4_Ear.py
+++ InMoov/services/4_Ear.py
@@ -11,6 +11,16 @@ i01.ear=Runtime.createAndStart("i01.ear", EarEngine)
 i01.startEar()
 ear = i01.ear
 
+
+# Start the webgui service without starting the browser
+webgui = Runtime.create("WebGui","WebGui")
+webgui.autoStartBrowser(False)
+webgui.startService()
+# Then start the browsers and show the WebkitSpeechRecognition service named i01.ear
+webgui.startBrowser("http://localhost:8888/#/service/i01.ear")
+# As an alternative you can use the line below to show all services in the browser. In that case you should comment out all lines above that starts with webgui. 
+# webgui = Runtime.createAndStart("webgui","WebGui")
+
 python.subscribe(ear.getName(),"recognized")
 chatBot=Runtime.create("chatBot", "ProgramAB")
 # ##############################################################################
