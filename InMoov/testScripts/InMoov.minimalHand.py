6999319718022bf7e549cb489df2a046d819ab21
Langevin Gael <hairygael@gmail.com>
2017-05-04 01:21:04 +0200

    Added auto install voice

--- InMoov/testScripts/InMoov.minimalHand.py
+++ InMoov/testScripts/InMoov.minimalHand.py
@@ -11,11 +11,11 @@
 rightPort = "COM7"
 
 #to tweak the default voice
-Voice="cmu-slt-hsmm" # Default female for MarySpeech 
-#Voice="cmu-bdl" #Male US voice.You need to add the necessary file.jar to myrobotlab.1.0.XXXX/library/jar
-#https://github.com/MyRobotLab/pyrobotlab/blob/ff6e2cef4d0642e47ee15e353ef934ac6701e713/home/hairygael/voice-cmu-bdl-5.2.jar
+Voice="cmu-bdl-hsmm" #Male US voice 
+#Voice="cmu-slt-hsmm" #Default female for MarySpeech
 voiceType = Voice
 mouth = Runtime.createAndStart("i01.mouth", "MarySpeech")
+mouth.installComponentsAcceptLicense(Voice)
 mouth.setVoice(voiceType)
 #i01.mouth.installComponentsAcceptLicense(Voice)
 ##############
