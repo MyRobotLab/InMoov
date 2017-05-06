49f111a8641f563dd6ffd5686e83bd6bbf767b9a
Anthony <moz4r@free.fr>
2017-05-06 19:47:57 +0200

    add battery to monitor

--- InMoov/life/HealthCheck.py
+++ InMoov/life/HealthCheck.py
@@ -9,18 +9,25 @@
 HealthCheck = Runtime.start("HealthCheck","Clock")
 HealthCheck.setInterval(60000)
 
+batterieLevel=100
+errorBat=1
 try:
+  if Runtime.getBatteryLevel():
     batterieLevel = Runtime.getBatteryLevel()
     print "battery :",batterieLevel
-    
+    errorBat=0
 except:
   pass
+
 
 def HealthCheck_def(timedata):
   global batterieLevel
 
-  if batterieLevel:batterieLevel = Runtime.getBatteryLevel()
- 
+  if not errorBat:
+    if Runtime.getBatteryLevel():batterieLevel = Runtime.getBatteryLevel()
+  i01.setBatteryLevel(int(batterieLevel))
+  print "battery :",batterieLevel
+  
   if RobotIsErrorMode==1:
     if error_red:
       PlayNeopixelAnimation("Flash Random", 255, 0, 0, 5)
--- InMoov/services/F_VirtualInmoov.py
+++ InMoov/services/F_VirtualInmoov.py
@@ -5,7 +5,7 @@
 if ScriptType=="Virtual":talkEvent(lang_startingVirtual)
 
 #virtual inmoov
-#i01.VinmoovMonitorActivated=1
+i01.VinmoovMonitorActivated=1
 #i01.VinmoovFullScreen=0
 #i01.VinmoovBackGroundColor="Grey"
 #i01.VinmoovWidth=800
