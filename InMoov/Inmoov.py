3b3cb0b5cae2f499d2b82625610d774a484a9d95
Anthony <moz4r@free.fr>
2017-05-16 01:45:20 +0200

    la météo
    todo weathrmap  english

--- InMoov/Inmoov.py
+++ InMoov/Inmoov.py
@@ -8,7 +8,7 @@
 version='0.3.5'
 
 # this will run with versions of MRL above :
-mrlCompatible='2168'
+mrlCompatible='2169'
 
 # ###################################################################################
 # This is a very minimal script for Inmoov
