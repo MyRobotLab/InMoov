94c1d6e56b9182a1d93f5e42da1573d0dc24e2cd
Langevin Gael <hairygael@gmail.com>
2017-05-31 20:35:58 +0200

    Create goodmorning.py

---
+++ InMoov/gestures/goodmorning/goodmorning.py
@@ -0,0 +1,9 @@
+def goodmorning():
+  i01.mouth.speak("good morning")
+  x = (random.randint(1, 3))
+  if x == 1:
+    i01.mouth.speak("i hope you had a good night sleep")
+  if x == 2:
+    i01.mouth.speak("nice to see you again")
+  if x == 3:
+    i01.mouth.speak("this is going to be a good day")
