2039da5ecb631a04926cf97e7dc1d6ab83bad0ae
Langevin Gael <hairygael@gmail.com>
2017-06-06 11:02:21 +0200

    Create rollHead.py

---
+++ InMoov/gestures/rollHead.py
@@ -0,0 +1,13 @@
+def rollHead():
+  i01.setHeadSpeed(0.75, 0.75)
+  i01.moveHead(90,90,20)
+  sleep(0.5)
+  i01.moveHead(90,90,170)
+  sleep(1)
+  i01.moveHead(90,90,20)
+  sleep(1)
+  i01.moveHead(90,90,170)
+  sleep(1)
+  i01.moveHead(90,90,90)
+  sleep(1)
+  i01.mouth.speakBlocking("thanks a lot, it feels great. doctor")
