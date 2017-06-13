52628d15991317a36ce85527b829c08c3e11b274
moz4r ( Anthony ) <moz4r@free.fr>
2017-06-13 16:10:08 +0200

    linux fixes - launcher

--- start_inmoov.sh
+++ start_inmoov.sh
@@ -1,32 +1,1 @@
-#!/bin/bash
-
-
-
-echo "------------------------------------------------------"
-echo "			INMOOV LAUNCHER"
-echo "------------------------------------------------------"
-
-echo "------------------------------------------------------"
-echo "LOG CLEAN UP to free space disk and send clean noworky"
-echo "------------------------------------------------------"
-
-rm myrobotlab.log.1
-mv myrobotlab.log myrobotlab.log.1
-
-echo "."
-echo "------------------------------------------------------"
-echo "UPDATE MRL INSTALLATION"
-echo "This may take a few minutes"
-echo "------------------------------------------------------"
-
-java -jar myrobotlab.jar -install
-
-echo "Done"
-echo "------------------------------------------------------"
-echo "START MRL & INMOOV"
-echo "------------------------------------------------------"
-
-clear
-# start the inmoov script
-java -jar myrobotlab.jar -invoke python execFile ./InMoov/InMoov.py -service python Python
-
+#!/bin/bashecho "------------------------------------------------------"echo "			INMOOV LAUNCHER"echo "------------------------------------------------------"echo "------------------------------------------------------"echo "LOG CLEAN UP to free space disk and send clean noworky"echo "------------------------------------------------------"rm myrobotlab.log.1mv myrobotlab.log myrobotlab.log.1echo "."echo "------------------------------------------------------"echo "UPDATE MRL INSTALLATION"echo "This may take a few minutes"echo "------------------------------------------------------"java -jar myrobotlab.jar -installecho "Done"echo "------------------------------------------------------"echo "START MRL & INMOOV"echo "------------------------------------------------------"clear#start the inmoov scriptjava -jar myrobotlab.jar -invoke python execFile ./InMoov/InMoov.py -service python Python
