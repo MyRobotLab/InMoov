0ed1e5e59a4d32dc3e66fa99d0a0fd9f6facfec0
Anthony <moz4r@free.fr>
2017-06-16 00:43:41 +0200

    fix voicerss utf8

--- InMoov/services/5_Mouth.py
+++ InMoov/services/5_Mouth.py
@@ -59,18 +59,18 @@ def publishMouthcontrolPinLeft(pins):
 def talk(data):
   if data:
     if data[0:2]=="l ":data=data.replace("l ", "l'")
-    if MyvoiceTTS!="VoiceRss":data=unicode(data,'utf-8') 
+    data=unicode(data,'utf-8') 
     mouth.speak(data)
     
 def talkBlocking(data):
   if data:
     if data[0:2]=="l ":data=data.replace("l ", "l'")
-    if MyvoiceTTS!="VoiceRss":data=unicode(data,'utf-8')
+    data=unicode(data,'utf-8')
     mouth.speakBlocking(data)
     
 def talkEvent(data):
   if IsMute==0:
-    if MyvoiceTTS!="VoiceRss":data=unicode(data,'utf-8')
+    data=unicode(data,'utf-8')
     subconsciousMouth.speakBlocking(data)
 
 #stop autolisten
