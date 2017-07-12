b5767b62167a697d2e894e85faebafa713f4b32c
Langevin Gael <hairygael@gmail.com>
2017-07-12 19:40:55 +0200

    Changed global bookcat to iReadbookcat

--- InMoov/gestures/bookcat.py
+++ InMoov/gestures/bookcat.py
@@ -20,56 +20,56 @@ def bookcatNo():
   i01.moveHead(90,90,90)
 
 def turnthepage(): 
-  global bookcat
-  if bookcat <= 0:
+  global iReadbookcat
+  if iReadbookcat <= 0:
     sleep(3)
     i01.mouth.speakBlocking("Cat the cat, who is that?")
-    bookcat += 1
-  elif bookcat == 1:
+    iReadbookcat += 1
+  elif iReadbookcat == 1:
     sleep(3)
     i01.mouth.speakBlocking("It's mouse, the mouse! Hi, mouse the mouse!  Hello there")
-    bookcat += 1
-  elif bookcat == 2:
+    iReadbookcat += 1
+  elif iReadbookcat == 2:
     sleep(3)
     i01.mouth.speakBlocking("Cat the cat, who is that?")
-    bookcat += 1
-  elif bookcat == 3:
+    iReadbookcat += 1
+  elif iReadbookcat == 3:
     sleep(3)
     i01.mouth.speakBlocking("It's duck the duck! Hi, duck the duck! A pleasure, as always")
-    bookcat += 1
-  elif bookcat == 4:
+    iReadbookcat += 1
+  elif iReadbookcat == 4:
     sleep(3)
     i01.mouth.speakBlocking("Cat the cat, who is that?")
-    bookcat += 1
-  elif bookcat == 5:
+    iReadbookcat += 1
+  elif iReadbookcat == 5:
     sleep(3)
     i01.mouth.speakBlocking("It's fish the fish! Hi, fish the fish! Hey dude")
-    bookcat += 1
-  elif bookcat == 6:
+    iReadbookcat += 1
+  elif iReadbookcat == 6:
     sleep(3)
     i01.mouth.speakBlocking("Cat the cat likes her friends. Sure do")
-    bookcat += 1
-  elif bookcat == 7:
+    iReadbookcat += 1
+  elif iReadbookcat == 7:
     sleep(3)
     i01.mouth.speakBlocking("Cat the cat, who is that?  eep")
-    bookcat += 1
-  elif bookcat == 8:
+    iReadbookcat += 1
+  elif iReadbookcat == 8:
     sleep(3)
     i01.mouth.speakBlocking("I have no idea.  blarggie! blarggie")
-    bookcat += 1
-  elif bookcat == 9:
+    iReadbookcat += 1
+  elif iReadbookcat == 9:
     sleep(3)
     i01.mouth.speakBlocking("Maybe")
-    bookcat += 1
-  elif bookcat == 10:
+    iReadbookcat += 1
+  elif iReadbookcat == 10:
     sleep(3)
     i01.mouth.speakBlocking("Blarggie! Blarggie!  It's a new friend! Blarggie! Blarggie")
-    bookcat += 1
-  elif bookcat == 11:
+    iReadbookcat += 1
+  elif iReadbookcat == 11:
     sleep(3)
     i01.mouth.speakBlocking("The end")
-    bookcat += 1
-  elif bookcat == 12:
+    iReadbookcat += 1
+  elif iReadbookcat == 12:
     sleep(3)
     i01.mouth.speakBlocking("The end")
-    bookcat += 1
+    iReadbookcat += 1
