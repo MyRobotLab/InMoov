655549e66b5337e3b36ad0e3ee05e085db5a9bd8
Langevin Gael <hairygael@gmail.com>
2017-09-07 17:47:34 +0200

    added i01.startedGesture()

--- InMoov/gestures/relax.py
+++ InMoov/gestures/relax.py
@@ -2,9 +2,8 @@ def relax():
   PlayNeopixelAnimation("Color Wipe", 0, 0, 20, 1)
   sleep(2)
   PlayNeopixelAnimation("Ironman", 0, 0, 255, 1)
-
+  i01.startedGesture()
   if (i01.RobotIsOpenCvCapturing()):
-    i01.disableRobotRandom(30)
     i01.setHandVelocity("left", 43, 43, 43, 43, 43, 43)
     i01.setHandVelocity("right", 43, 43, 43, 43, 43, 43)
     i01.setArmVelocity("right", 31, 43, 23, 43)
@@ -31,3 +30,4 @@ def relax():
     i01.moveHand("left",92,33,37,71,66,25)
     i01.moveHand("right",81,66,82,60,105,113)
     i01.moveTorso(95,90,90)
+  i01.finishedGesture()
