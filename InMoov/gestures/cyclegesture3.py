e46157a8b59d4e2bb369152d04273e422a1377be
Langevin Gael <hairygael@gmail.com>
2017-10-02 00:32:59 +0200

    Modified the position of i01.startedGesture()

--- InMoov/gestures/cyclegesture3.py
+++ InMoov/gestures/cyclegesture3.py
@@ -2,8 +2,8 @@
 
 def cyclegesture3():
   ##for x in range(3):
-  i01.startedGesture()
   rest()
+  i01.startedGesture()
   i01.setHandSpeed("left", 0.85, 0.85, 0.85, 0.85, 0.85, 1.0)
   i01.setHandSpeed("right", 0.85, 0.85, 0.85, 0.85, 0.85, 1.0)
   i01.setArmSpeed("left", 1.0, 1.0, 1.0, 1.0)
@@ -178,8 +178,8 @@ def cyclegesture3():
   i01.mouth.speakBlocking("wow, I feel good, I love this")
   #i01.mouth.speakBlocking(u"Ничего себе, я чувствую себя хорошо, мне это нравится")
   sleep(2)
+  i01.finishedGesture()
   rest()
   sleep(1)
   relax()
   sleep(3)
-  i01.finishedGesture()
