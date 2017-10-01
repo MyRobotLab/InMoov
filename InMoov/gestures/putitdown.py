8a63a509a5c1d87f0051608baeeea33b6a85cfaa
Langevin Gael <hairygael@gmail.com>
2017-10-02 00:12:44 +0200

    Added i01.startedGesture()

--- InMoov/gestures/putitdown.py
+++ InMoov/gestures/putitdown.py
@@ -1,4 +1,5 @@
 def putitdown():
+  i01.startedGesture()
   i01.setHandSpeed("left", 0.90, 0.90, 0.90, 0.90, 0.90, 0.90)
   i01.setHandSpeed("right", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
   i01.setArmSpeed("right", 0.75, 0.85, 0.95, 0.85)
@@ -9,4 +10,6 @@ def putitdown():
   i01.moveArm("right",5,82,33,15)
   i01.moveHand("left",147,130,135,34,34,35)
   i01.moveHand("right",20,40,40,30,30,72)
+  sleep(1)
+  i01.finishedGesture()
 
