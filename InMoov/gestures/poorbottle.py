bbe409cb4a7708ada8d166468b6d1e0d87c2644c
Langevin Gael <hairygael@gmail.com>
2017-10-02 10:50:03 +0200

    Added i01.startedGesture()

--- InMoov/gestures/poorbottle.py
+++ InMoov/gestures/poorbottle.py
@@ -1,4 +1,5 @@
 def poorbottle():
+  i01.startedGesture()
   i01.setHandSpeed("left", 0.60, 0.60, 0.60, 0.60, 0.60, 0.60)
   i01.setHandSpeed("right", 0.60, 0.80, 0.60, 0.60, 0.60, 0.60)
   i01.setArmSpeed("left", 0.60, 0.60, 0.65, 0.60)
@@ -11,4 +12,6 @@ def poorbottle():
   i01.moveHand("left",180,140,150,164,180,0)
   i01.moveHand("right",145,112,127,105,143,150)
   i01.moveTorso(90,90,90)
+  sleep(1)
+  i01.finishedGesture()
 
