5792331daf61add98eaa2ec5af5be4d3d953f1a4
Langevin Gael <hairygael@gmail.com>
2017-10-02 11:04:27 +0200

    Added i01.startedGesture()

--- InMoov/gestures/removeleftarm.py
+++ InMoov/gestures/removeleftarm.py
@@ -1,4 +1,5 @@
 def removeleftarm():
+  i01.startedGesture()
   i01.setHandSpeed("left", 0.85, 0.85, 0.85, 0.85, 0.85, 0.85)
   i01.setHandSpeed("right", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
   i01.setArmSpeed("right", 0.75, 0.85, 0.95, 0.85)
@@ -9,5 +10,7 @@ def removeleftarm():
   i01.moveArm("right",5,82,28,15)
   i01.moveHand("left",60,43,45,34,34,35)
   i01.moveHand("right",20,40,40,30,30,72)
+  sleep(1)
+  i01.finishedGesture()
 
 
