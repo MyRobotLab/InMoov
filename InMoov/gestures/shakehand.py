6fa8adb3acb810aeb680ab494e342b0505e0bd87
Anthony <moz4r@free.fr>
2017-10-10 02:28:07 +0200

    optional distance sensor for shake hand

--- InMoov/gestures/shakehand.py
+++ InMoov/gestures/shakehand.py
@@ -17,7 +17,53 @@ def shakehand():
   i01.moveHand("right",50,50,40,20,20,90)
   i01.moveTorso(95,95,90)
   sleep(3)
+  ##set hand up
+  i01.setHandSpeed("left", 0.65, 0.65, 0.65, 0.65, 0.65, 1.0)
+  i01.setHandSpeed("right", 0.85, 0.85, 0.85, 0.85, 0.85, 1.0)
+  i01.setArmSpeed("right", 1.0, 1.0, 1.0, 1.0)
+  i01.setArmSpeed("left", 1.0, 1.0, 1.0, 1.0)
+  i01.setHeadSpeed(1.0, 1.0)
+  i01.setTorsoSpeed(0.75, 0.55, 1.0)
+  i01.moveHead(90,90)
+  i01.moveArm("left",5,84,16,15)
+  i01.moveArm("right",6,73,60,16)
+  i01.moveHand("left",50,50,40,20,20,90)
+  i01.moveTorso(95,95,90)
+  i01.moveHand("right",80,70,70,50,50,77)
+  ##ask chatbot to say something
+  chatBot.getResponse("SYSTEM_SHAKE_HAND_GET_CLOSER")
+   ##optional, detect if there is a human ( waiting finger sensor, we use ultrasonic distance as test )
+  if ultraSonicSensorActivated:
+    distance=200
+    timeout=0
+    timeoutGetCloser=0
+    while (not distance or distance > 100): 
+      timeout+=1
+      timeoutGetCloser+=1
+      distance=i01.getUltrasonicSensorDistance()
+      print distance
+      if timeout > 20:
+        chatBot.getResponse("SYSTEM_SHAKE_HAND_NOHUMAN")
+        sleep(2)
+        shakehandFinish()
+        break
+      # ask GET CLOSER
+      if timeoutGetCloser>6:
+        chatBot.getResponse("SYSTEM_SHAKE_HAND_GET_CLOSER")
+        timeoutGetCloser=0
+        sleep(2)
+      sleep(0.5)
+      # great, human detected
+    if distance<=100:shakehandAnimation()
+  else:
+    sleep(3)
+    shakehandAnimation()
+
+def shakehandAnimation():
+  chatBot.getResponse("SYSTEM_SHAKE_HAND")
   ##close the hand
+  i01.moveHand("right",180,125,135,145,168,77)
+  sleep(1)
   i01.setHandSpeed("left", 0.65, 0.65, 0.65, 0.65, 0.65, 1.0)
   i01.setHandSpeed("right", 0.85, 0.85, 0.85, 0.85, 0.85, 1.0)
   i01.setArmSpeed("right", 0.75, 0.85, 0.95, 0.85)
@@ -31,43 +77,6 @@ def shakehand():
   i01.moveHand("right",180,125,135,145,168,77)
   i01.moveTorso(95,95,90)
   sleep(3)
-  ##shake hand up
-  i01.setHandSpeed("left", 0.65, 0.65, 0.65, 0.65, 0.65, 1.0)
-  i01.setHandSpeed("right", 0.85, 0.85, 0.85, 0.85, 0.85, 1.0)
-  i01.setArmSpeed("right", 1.0, 1.0, 1.0, 1.0)
-  i01.setArmSpeed("left", 1.0, 1.0, 1.0, 1.0)
-  i01.setHeadSpeed(1.0, 1.0)
-  i01.setTorsoSpeed(0.75, 0.55, 1.0)
-  i01.moveHead(90,90)
-  i01.moveArm("left",5,84,16,15)
-  i01.moveArm("right",6,73,60,16)
-  i01.moveHand("left",50,50,40,20,20,90)
-  i01.moveHand("right",180,125,135,145,168,77)
-  i01.moveTorso(95,95,90)
-  sleep(1)
-  ##shake hand down
-  x = (random.randint(1, 7))
-  if x == 1:
-    i01.mouth.speak("please to meet you")
-    #i01.mouth.speak(u"рад встрече с вами")
-  if x == 2:
-    i01.mouth.speak("carefull my hand is made out of plastic")
-    #i01.mouth.speak(u"Осторожно, моя рука сделана из пластика")
-  if x == 3:
-    i01.mouth.speak("I am happy to shake a human hand")
-    #i01.mouth.speak(u"Я рад пожать руку человеку")
-  if x == 4:
-    i01.mouth.speak("it is a pleasure to meet you")
-    #i01.mouth.speak(u"Мне приятно  встретить тебя")
-  if x == 5:
-    i01.mouth.speak("very nice to meet you") 
-    #i01.mouth.speak(u"приятно познакомиться")
-  if x == 6:
-    i01.mouth.speak("shaking a human hand, feels strange. please to meet you") 
-    #i01.mouth.speak(u"Пожимая человеческую руку, чувствую себя странно. Рад встрече с вами.")
-  if x == 7:
-    i01.mouth.speak("I am glad you don't shake me like trump does")
-    #i01.mouth.speak(u"Я рад, что ты не встряхиваешь мою руку, как козырь")    
   i01.setHandSpeed("left", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
   i01.setHandSpeed("right", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
   i01.setArmSpeed("left", 1.0, 1.0, 1.0, 1.0)
@@ -79,6 +88,34 @@ def shakehand():
   i01.moveHead(39,70)
   sleep(0.5)
   i01.moveHead(70,53)
+  i01.moveArm("left",5,84,16,15)
+  i01.moveArm("right",6,73,48,16)
+  i01.moveHand("left",50,50,40,20,20,90)
+  i01.moveHand("right",180,125,135,145,168,77)
+  i01.moveTorso(95,95,90)
+  sleep(1.5)
+  ##shake hand up
+  i01.setHandSpeed("left", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+  i01.setHandSpeed("right", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+  i01.setArmSpeed("left", 1.0, 1.0, 1.0, 1.0)
+  i01.setArmSpeed("right", 1.0, 1.0, 1.0, 1.0)
+  i01.setHeadSpeed(0.85, 0.85)
+  i01.setTorsoSpeed(0.65, 0.55, 1.0)
+  i01.moveHead(80,53)
+  i01.moveArm("left",5,84,16,15)
+  i01.moveArm("right",6,73,60,16)
+  i01.moveHand("left",50,50,40,20,20,90)
+  i01.moveHand("right",180,125,135,145,168,77)
+  i01.moveTorso(95,95,90)
+  sleep(1)
+  ##shake hand down
+  i01.setHandSpeed("left", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+  i01.setHandSpeed("right", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+  i01.setArmSpeed("left", 1.0, 1.0, 1.0, 1.0)
+  i01.setArmSpeed("right", 1.0, 1.0, 1.0, 1.0)
+  i01.setHeadSpeed(0.85, 0.85)
+  i01.setTorsoSpeed(0.65, 0.55, 1.0)
+  i01.moveHead(80,88)
   i01.moveArm("left",5,84,16,15)
   i01.moveArm("right",6,73,48,16)
   i01.moveHand("left",50,50,40,20,20,90)
@@ -127,6 +164,9 @@ def shakehand():
   i01.moveHand("right",20,30,30,20,20,90)
   i01.moveTorso(95,95,90)
   sleep(1)
+  shakehandFinish()
+
+def shakehandFinish():
   i01.finishedGesture()
   rest()
   relax()
