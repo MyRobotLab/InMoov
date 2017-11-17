f93eb4df8f68acdb6af7488addba860be1282504
Anthony <moz4r@free.fr>
2017-11-17 01:06:05 +0100

    noFaceRecognizerOverride

--- InMoov/gestures/trackHumans.py
+++ InMoov/gestures/trackHumans.py
@@ -1,4 +1,4 @@
-def trackHumans():
+def trackHumans(noFaceRecognizerOverride=True):
   stopTracking()
   i01.startHeadTracking("leftPort",12,13)
   #i01.startEyesTracking("leftPort",22,24)
@@ -6,5 +6,8 @@ def trackHumans():
   #i01.eyesTracking.pid.setPID("eyeY",eyeYPidKp,eyeYPidKi,eyeYPidKd)
   i01.headTracking.pid.setPID("x",rotheadPidKp,rotheadPidKi,rotheadPidKd)
   i01.headTracking.pid.setPID("y",neckPidKp,neckPidKi,neckPidKd)
-  fr=i01.headTracking.faceDetect(faceRecognizerActivated)
-  if faceRecognizerActivated:fr.train()# it takes some time to train and be able to recognize face
+  if noFaceRecognizerOverride:
+    fr=i01.headTracking.faceDetect(faceRecognizerActivated)
+    if faceRecognizerActivated:fr.train()# it takes some time to train and be able to recognize face
+  else:
+    i01.headTracking.faceDetect(False)
