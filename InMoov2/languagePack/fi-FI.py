e980bfa99fdedce78462f27056dee96cb3007610
Anthony <moz4r@free.fr>
2018-06-09 14:21:30 +0200

    fi  lang mod

---
+++ InMoov2/languagePack/fi-FI.py
@@ -0,0 +1,33 @@
+Hälytys, voinko kiinnittää huomionne!
+On olemassa ongelma! viestintäporttini, tarkista arduino
+Bad M R L com -versio sisällä arduino, päivitä
+Alertti, aivoni on vioittunut, ehkä huono shutdown?
+Chatbot ladattu, voin käynnistää muita komponentteja, odota
+Chatbot ei voi käynnistää, koska en löytänyt sanastoa omasta kielestäsi
+Minä lataan aivoni, odota
+Suun synkronointi automaattisesti äänisignaalin perusteella
+En havaitsi yhtään hyvää äänisignaalia analogisessa portissa,
+Tunnistan äänisignaalin, mutta se ei ole täydellinen Tarkista aref arduino, älä huoli siitä, että se on työläs
+Nizza, katso huuliani, he liikkuvat
+ongelma on neopikselin kanssa, tarkista yhteys ja m r l com -protokolla
+Avaa Ceve ei voi käynnistää, ongelma,
+Kinect avoinna n, ei voi aloittaa. Tarkista microsoft kinect s. d. k.
+Ok, sinulla on minun huomioni
+Sammuta järjestelmäni, odota 10 sekuntia
+Korvien aloitus
+Aloittaminen silmäluomien
+Aloituspään
+Vasemman käden aloitus
+Vasemmanpuoleinen käsi
+Aloittavan suun
+Aloita neopikselipalvelu
+avaa c.v. kamera, valmis
+Kinect, aloitettu
+p, i, r anturin aktivointi
+Oikean käden aloitus
+Oikeanpuoleinen käsi
+Aloita telan kaula
+Aloitus Torso
+Käynnistys ultraäänianturilla
+virtuaalinen inmoov aktivoitu
+aloittavan seurannan
