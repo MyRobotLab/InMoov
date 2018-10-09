aa5c565ef2b23737253c3f9b0e6e2ea27902e5aa
Langevin Gael <hairygael@gmail.com>
2018-10-09 22:18:16 +0200

    Update darknet.py

--- InMoov/gestures/darknet.py
+++ InMoov/gestures/darknet.py
@@ -1,21 +1,24 @@
 def darknet():  
   imagedisplay.closeAll()
   sleep(0.5)
-  if (i01.opencv.capture()):
-    i01.setHeadVelocity(-1,-1,-1,-1,-1,-1)
-    i01.moveHeadBlocking(75,90,90,30,0,90)
-    sleep(0.5)
-    takeFotoForYolo()
-    statisticResult()
-    sleep(0.1)
-    analyseResult()
-  else:
-    i01.opencv.capture()
-    sleep(1)
-    i01.setHeadVelocity(-1,-1,-1,-1,-1,-1)
-    i01.moveHeadBlocking(75,90,90,30,0,90)
-    sleep(0.5)
-    takeFotoForYolo()
-    statisticResult()
-    sleep(0.1)
-    analyseResult()
+  if isNeopixelActivated==1:
+        i01.setNeopixelAnimation("Color Wipe", 25, 5, 10, 15)
+  if isOpenCvActivated==1:  
+    if i01.RobotIsOpenCvCapturing():
+      i01.setHeadVelocity(-1,-1,-1,-1,-1,-1)
+      i01.moveHeadBlocking(75,90,90,30,0,90)
+      sleep(1)
+      takeFotoForYolo()
+      statisticResult()
+      sleep(0.1)
+      analyseResult()
+    else:
+      i01.opencv.capture()
+      sleep(1)
+      i01.setHeadVelocity(-1,-1,-1,-1,-1,-1)
+      i01.moveHeadBlocking(75,90,90,30,0,90)
+      sleep(1)
+      takeFotoForYolo()
+      statisticResult()
+      sleep(0.1)
+      analyseResult()
