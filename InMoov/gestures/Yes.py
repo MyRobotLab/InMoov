ae7f9f5cb64a4c36129ce634ac8022b655341a9c
Langevin Gael <hairygael@gmail.com>
2018-10-14 17:26:47 +0200

    Update Yes.py

--- InMoov/gestures/Yes.py
+++ InMoov/gestures/Yes.py
@@ -1,10 +1,12 @@
 def Yes():
   if isHeadActivated==1:
     i01.startedGesture()
-    i01.setHeadVelocity(45,45,45)
-    i01.moveHeadBlocking(130,90)
-    i01.moveHeadBlocking(50,93)
-    i01.moveHeadBlocking(120,88)
-    i01.moveHeadBlocking(70,90)
-    i01.moveHeadBlocking(90,90)
+    i01.setHeadVelocity(80,80,80)
+    i01.moveHead(50,93)
+    sleep(0.3)
+    i01.moveHead(120,88)
+    sleep(0.3)
+    i01.moveHead(70,90)
+    sleep(0.4)
+    i01.moveHead(90,90)
     i01.finishedGesture()
