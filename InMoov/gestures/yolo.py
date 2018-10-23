0fbd2ef0954d9a95faa992fac7e07935cdb7665e
Anthony <moz4r@free.fr>
2018-10-24 01:16:00 +0200

    yolo filter poc
    https://github.com/MyRobotLab/inmoov/issues/180

--- InMoov/gestures/yolo.py
+++ InMoov/gestures/yolo.py
@@ -1,103 +1,56 @@
-# -- coding: utf-8 --
+## Yolo gesture poc
+# Cumulative frames analysis :
+# We count objetcs per frame and get the maximum detected for 1 frame only
+# So we can list & count at one time, every available classified object in the field of view, in given time
 
-from java.lang import String
-from org.myrobotlab.service import Runtime
-from org.myrobotlab.service import OpenCV
-from time import sleep
-from shutil import copyfile
-import os, sys
-
-list=[]
-list2=[]
-cpt=0
-
-#We need to start the camera before launching yolo
-#this break inmoov1 from nixie
-#yolo = Runtime.createAndStart("yolo","Yolo")
-#ImageYolo = Runtime.createAndStart("ImageYolo", "ImageDisplay")
-
-# ##############################################################################
-def displayPic(pic):
-  r = ImageYolo.displayFullScreen(pic)
-
-# ##############################################################################
-def takeFotoForYolo():
-  print "Photo..."
-  os.chdir("c:\yolo")
-
-  photoFileName = opencv.recordSingleFrame()
-  print photoFileName
+# collection is the dictionary for detected objects in given time
+# after aquisition, we will read collection content
+collection={}
+def startYoloInventory(duration):
+  talk(chatBot.getPredicate(chatBot.getCurrentUserName(),"startupSentence"))
+  global collection
+  collection={}
+  #start opencv + yolo filter
+  i01.opencv.capture()
+  i01.opencv.removeFilters()
+  i01.opencv.addFilter("PyramidDown")
+  i01.opencv.addFilter("Yolo")
+  # wait for X ( todo unlimited, until STOP vocal command ? )
+  sleep(duration)
+  i01.opencv.removeFilters()
+  i01.opencv.stopCapture()
+  # class results ...
+  collectionString=""
+  for key, value in collection.iteritems():
+    collectionString=collectionString+str(value)+" "+str(key)+", "
+  #check if we have results, return key "none" if no ( aiml will understand the key )
+  print collectionString
+  if len(collectionString) == 0:collectionString="none"
+  return collectionString
   
-  os.remove("image.jpg")
-  os.rename(photoFileName,"image.jpg")
-  copyfile("image.jpg", "c:\yolo\yolo\image.jpg")
-  sleep(0.1)
 
-  yolo.execYolo()
-
-# ##############################################################################
-def statisticResult():
-  global list
-  NbElement=0
+## OpenCV configuration for yolo publisher
   
-  print "statistique..."
-  res = yolo.StatisticResult()
+python.subscribe("i01.opencv", "publishYoloClassification")  
+# TODO classify while head moves ! ( count )
+# TODO what do you see in front of you ( few centimeters -> use gael mods )
+# TODO tranlate coco
+# TODO ...
 
-  if res == True:
-    # Ici il y a obligatoirement des objects
-    list=[]
-    file = open("statistics.txt","r")
-	
-    ImageYolo.display("c:\yolo\yolo\predictions.jpg")
-	
-    for ligne in file:
-      list.append(ligne)
-    file.close()
-    
-    NbElement=len(list)
-    print "Number of recognized elements:", NbElement
+def onYoloClassification(data):
+  global collection
 
-    #i01.mouth.speak("I see")
-
-    for val in list:
-      print val
-      #i01.mouth.speak(unicode(val, 'utf-8'))
-      #sleep(2)
-  else:
-    print "Nothing found !!"
-    #i01.mouth.speak("I see nothing")
-  
-# ##############################################################################
-def analyseResult():
-  global list
-  NbElement=0
-  #displayPic("c:\yolo\yolo\predictions.jpg")
-  print "analyse..."
-  res = yolo.AnalyseResult()
-
-  if res == True:
-    # Ici il y a obligatoirement des objects
-    list=[]
-    file = open("finalresult.txt","r")
-
-    for ligne in file:
-      list.append(ligne)
-    file.close()
-    
-    NbElement=len(list)
-	
-    print "Number of recognized elements:", NbElement
-
-    i01.mouth.speak("I see")
-  
-    for val in list:
-      print val
-      i01.mouth.speak(unicode(val, 'utf-8'))
-      sleep(2)
-  else:
-    print "Nothing found !!"
-    i01.mouth.speak("I see nothing")
-
-##################################################################################
-# Timer ...
-##################################################################################
+  label=[]
+  # iterate over the frame and extract classified objects ( TODO this inside java land to publish it ? )
+  for x in data:
+    object=str(x).split(",")[3].replace("label=","")
+    label.append(object)
+  # count objects detected
+  for key, group in itertools.groupby(label):
+    if key in collection:
+      #we append exiting object only if we found a greater value ( by exemple there is 3 persons in the room and the 3rd was hidden from now )
+      if int(collection[key]) <= label.count(key):
+      	collection[key]=label.count(key)
+    else:
+      # object never detected
+      collection[key]=label.count(key)
