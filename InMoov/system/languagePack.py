2f882eadf8f04230c526e3cd0d9fc6b1a7171bf8
Anthony <moz4r@free.fr>
2018-11-05 23:34:02 +0100

    coco.names globalized
    https://github.com/MyRobotLab/myrobotlab/issues/289

--- InMoov/system/languagePack.py
+++ InMoov/system/languagePack.py
@@ -16,12 +16,16 @@ for root, subdirs, files in os.walk(RuningFolder+'system/languagePack/en'):
   
 if (os.path.isdir(RuningFolder+'system/languagePack/'+languagePack)):
   try:
+    #push local yolo dictionary
+    shutil.copy(RuningFolder+'system/languagePack/'+languagePack+'/coco.names',os.getcwd().replace("\\", "/")+"/yolo/coco.names")
+    #other translations 
     for root, subdirs, files in os.walk(RuningFolder+'system/languagePack/'+languagePack):
       for name in files:
         if name.split(".")[-1] == "lang":
           execfile(os.path.join(root, name).encode('utf8'))
           if DEBUG==1:print "debug user languagePack : ",os.path.join(root, name)
     
+  
   except:
     languagePackLoaded=0
     pass
