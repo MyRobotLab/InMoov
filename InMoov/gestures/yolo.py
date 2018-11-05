ce5443e37e3ef577eb4a7921005b3b168ef99ee0
Anthony <moz4r@free.fr>
2018-11-05 01:26:24 +0100

    get yolo element position from others

--- InMoov/gestures/yolo.py
+++ InMoov/gestures/yolo.py
@@ -1,32 +1,34 @@
-## Yolo gesture poc
-# Cumulative frames analysis :
-# We count objetcs per frame and get the maximum detected for 1 frame only
+## Yolo filter / 2 samples
+# We classify objects per frame and get the maximum detected for 1 frame only
 # So we can list & count at one time, every available classified object in the field of view, in given time
+# "collection" is the dictionary for detected objects in given time
+# after aquisition, we will read collection content to play with it
 
-# collection is the dictionary for detected objects in given time
-# after aquisition, we will read collection content
-collection={}
+
+# Cumulative frames analysis :
+collection=[]
 def startYoloInventory(duration):
   talk(chatBot.getPredicate(chatBot.getCurrentUserName(),"startupSentence"))
-  global collection
-  collection={}
-  #start opencv + yolo filter
-  i01.opencv.capture()
-  i01.opencv.removeFilters()
-  i01.opencv.addFilter("PyramidDown")
-  i01.opencv.addFilter("Yolo")
-  # wait for X ( todo unlimited, until STOP vocal command ? )
-  sleep(duration)
-  i01.opencv.removeFilters()
-  i01.opencv.stopCapture()
-  # class results ...
+  startYolo(duration)
+  # interpret results ...
   collectionString=""
-  for key, value in collection.iteritems():
+  for key, value in countYolo().iteritems():
     collectionString=collectionString+str(value)+" "+str(key)+", "
   #check if we have results, return key "none" if no ( aiml will understand the key )
-  print collectionString
+  print collection
   if len(collectionString) == 0:collectionString="none"
   return collectionString
+
+  
+# function to get given element (label) position, from all others in the field off view, from left to right
+# todo add filter ( like "table" for elements onto )
+def getYoloPosition(duration,label):
+  talk(chatBot.getPredicate(chatBot.getCurrentUserName(),"startupSentence"))
+  startYolo(duration)
+  position=0
+  for x in sortLeftRightYolo():
+    if x[0]==label:position=sortLeftRightYolo().index(x)+1
+  return position
   
 
 ## OpenCV configuration for yolo publisher
@@ -37,20 +39,52 @@ python.subscribe("i01.opencv", "publishYoloClassification")
 # TODO tranlate coco
 # TODO ...
 
+#main function to read yolo filter output
 def onYoloClassification(data):
   global collection
-
   label=[]
+  
+  # iterate over the frame and clean up previous results based on actual detection ( update positions + count elements )
+  # todo cleaner method to extract data...
+  for x in data:
+    object=str(x).split(",")[3].replace("label=","").strip()
+    for y in collection:
+      if object in y:
+        collection.remove(y)
+        break
+
   # iterate over the frame and extract classified objects ( TODO this inside java land to publish it ? )
   for x in data:
-    object=str(x).split(",")[3].replace("label=","")
-    label.append(object)
-  # count objects detected
-  for key, group in itertools.groupby(label):
-    if key in collection:
-      #we append exiting object only if we found a greater value ( by exemple there is 3 persons in the room and the 3rd was hidden from now )
-      if int(collection[key]) <= label.count(key):
-      	collection[key]=label.count(key)
+    object=str(x).split(",")[3].replace("label=","").strip()
+    xPos=str(x).split(",")[0].replace("YoloDetectedObject [boundingBox=X:","") 
+    collection.append([object,xPos])      
+
+#shared function to count classified elements
+def countYolo():
+  yoloDic={}
+  for yoloLabel in collection:
+    if yoloLabel[0] in yoloDic:
+      yoloDic[yoloLabel[0]]=yoloDic[yoloLabel[0]]+1
     else:
-      # object never detected
-      collection[key]=label.count(key)
+      yoloDic[yoloLabel[0]]=1
+  return yoloDic
+  
+#shared function to start & stop yolo filter
+def startYolo(duration):
+  global collection
+  collection=[]
+  #start opencv + yolo filter
+  i01.opencv.capture()
+  i01.opencv.removeFilters()
+  i01.opencv.addFilter("PyramidDown")
+  i01.opencv.addFilter("Yolo")
+  # wait for X ( todo unlimited, until STOP vocal command ? )
+  sleep(duration)
+  i01.opencv.removeFilters()
+  i01.opencv.stopCapture()
+  print collection
+  
+#shared function to sort elements from left to right
+def sortLeftRightYolo():
+  sortedObject = sorted(collection, key=lambda k: int(k[1]))
+  return sortedObject
