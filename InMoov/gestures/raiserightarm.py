cb95c2ee988991239e95cbd46e38ec9f10c5a8c9
Langevin Gael <hairygael@gmail.com>
2018-12-14 14:21:14 +0100

    Create raiserightarm.py

---
+++ InMoov/gestures/raiserightarm.py
@@ -0,0 +1,10 @@
+def raiserightarm():
+  i01.startedGesture()
+  i01.moveArm("left",5.00,90.00,30.00,12.00)
+  i01.moveArm("right",57.00,51.00,30.00,33.00)
+  i01.moveHand("left",0.00,0.00,0.00,0.00,0.00,90.00)
+  i01.moveHand("right",0.00,0.00,0.00,0.00,0.00,177.00)
+  i01.moveTorso(90.00,90.00,90.00)
+  sleep(5)
+  i01.finishedGesture()
+  relax()
