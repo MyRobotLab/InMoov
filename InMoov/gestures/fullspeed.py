7e7b158d9c38039441b809cf2292f2e90df82f7e
Langevin Gael <hairygael@gmail.com>
2019-01-31 08:34:13 +0100

    fix for neopixel

--- InMoov/gestures/fullspeed.py
+++ InMoov/gestures/fullspeed.py
@@ -1,7 +1,8 @@
 def fullspeed():
-  i01.setNeopixelAnimation("Color Wipe", 200, 0, 0, 1)
-  sleep(1)
-  i01.setNeopixelAnimation("Ironman", 0, 0, 255, 1)
+  if isNeopixelActivated:
+    i01.setNeopixelAnimation("Color Wipe", 200, 0, 0, 1)
+    sleep(1)
+    i01.setNeopixelAnimation("Ironman", 0, 0, 255, 1)
   if isRightHandActivated:
     i01.setHandVelocity("right", -1, -1, -1, -1, -1, -1)
       
