16899a63dc8e1e8ee6f28a82c36852de362627c4
Langevin Gael <hairygael@gmail.com>
2019-03-13 21:28:23 +0100

    Create memorizePerson.py

---
+++ InMoov/gestures/memorizePerson.py
@@ -0,0 +1,14 @@
+def memorizePerson():
+  i01.chatBot.getResponse("SAY " + "What name should I memorize for this person")
+
+def YesName():
+  print "name confirmed"
+  if isNeopixelActivated==1:
+    i01.setNeopixelAnimation("Color Wipe", 100, 5, 10, 15) 
+  if isChatbotActivated:
+    i01.cameraOn()
+    fr=i01.vision.setActiveFilter("FaceRecognizer")
+    #TODO set name of the person to be trained via the AIML
+    fr.train()
+    sleep(2)
+    i01.opencv.disableFilter("FaceRecognizer")
