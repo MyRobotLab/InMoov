6a3e872f865153f2d3d2bbfbae3731eda0126315
Kevin Watters <kwatters@kmwllc.com>
2019-03-13 17:00:32 -0400

    Update memorizePerson.py
    allowing the name to be passed into the YesName method. using that to set the trainName on the filter.. set the mode to TRAIN mode.  let it accumulate examples .  and finally train a new model and once done start recognizing again

--- InMoov/gestures/memorizePerson.py
+++ InMoov/gestures/memorizePerson.py
@@ -1,15 +1,25 @@
+# I'm not sure if you need this here or not.
+from org.myrobotlab.opencv import OpenCVFilterFaceRecognizer
+
 def memorizePerson():
   i01.chatBot.getResponse("SAY " + "What name should I memorize for this person")
   #this sends back to the inmoovGestures.AIML
 
-def YesName():
+def YesName(name):
   print "name confirmed"
   if isNeopixelActivated==1:
     i01.setNeopixelAnimation("Color Wipe", 100, 5, 10, 15) 
   if isChatbotActivated:
     i01.cameraOn()
     fr=i01.vision.setActiveFilter("FaceRecognizer")
-    #TODO set name of the person to be trained via the AIML
+    # set the name on the filter that will be used for the saved examples
+    fr.trainName = name
+    # set the filter to be in training mode (Where it learns new images)
+    fr.setMode(OpenCVFilterFaceRecognizer.Mode.TRAIN)
+    # wait 5 seconds for the example images to be taken.
+    sleep(5)
+    # now that we have new examples, let's re-train the face recognizer with all our examples.
     fr.train()
-    sleep(2)
+    # after we've retrained the model.. start recognizing again
+    fr.setMode(OpenCVFilterFaceRecognizer.Mode.RECOGNIZE);
     i01.opencv.disableFilter("FaceRecognizer")
