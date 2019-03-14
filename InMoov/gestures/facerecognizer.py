ac98e23f64d0bcbb77e5273db72733359d230f64
Langevin Gael <hairygael@gmail.com>
2019-03-14 16:20:33 +0100

    Update facerecognizer.py

--- InMoov/gestures/facerecognizer.py
+++ InMoov/gestures/facerecognizer.py
@@ -2,4 +2,4 @@ def facerecognizer():
   #you need to train at least 2 FACES !   
   i01.cameraOn()
   fr=i01.vision.setActiveFilter("FaceRecognizer")
-  fr.train()# it takes some time to train and be able to recognize face
+  #fr.train()# it takes some time to train and be able to recognize face
