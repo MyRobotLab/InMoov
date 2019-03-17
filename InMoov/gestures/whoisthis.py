a1db1a3a6ac488d855f5b56de1740447373f64f7
Langevin Gael <hairygael@gmail.com>
2019-03-18 00:35:43 +0100

    Update whoisthis.py

--- InMoov/gestures/whoisthis.py
+++ InMoov/gestures/whoisthis.py
@@ -2,18 +2,21 @@ def whoisthis():
   if isNeopixelActivated==1:
     i01.setNeopixelAnimation("Color Wipe", 100, 5, 10, 15) 
   if isChatbotActivated:
-    i01.cameraOn()
-    fr=i01.vision.setActiveFilter("FaceRecognizer")
-    # set the name on the filter that will be used for the saved examples
-    #fr.setMode(OpenCVFilterFaceRecognizer.Mode.TRAIN)
-    fr.train#Name = name
-    fr.setMode(OpenCVFilterFaceRecognizer.Mode.RECOGNIZE)
-    sleep(6)
-    i01.setHeadVelocity(70, 70, 70)
-    i01.moveHead(90,90,20)
-    sleep(1.3)
-    i01.moveHead(90,90,170)
-    sleep(2)
-    i01.moveHead(90,90,90)
-    onRecognizedFace(name)
+    if isOpenCvActivated==1:
+      i01.cameraOn()
+      fr=i01.vision.setActiveFilter("FaceRecognizer")
+      # set the name on the filter that will be used for the saved examples
+      #fr.setMode(OpenCVFilterFaceRecognizer.Mode.TRAIN)
+      fr.train#Name = name
+      fr.setMode(OpenCVFilterFaceRecognizer.Mode.RECOGNIZE)
+      sleep(6)
+      i01.setHeadVelocity(70, 70, 70)
+      i01.moveHead(90,90,20)
+      sleep(1.3)
+      i01.moveHead(90,90,170)
+      sleep(2)
+      i01.moveHead(90,90,90)
+      onRecognizedFace(name)
+    else:
+      errorSpokenFunc('OpenCvNoWorky')
 
