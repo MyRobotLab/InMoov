ec1ba81024b85cee587f3fdb4e1ebdec1b37b3ad
Langevin Gael <hairygael@gmail.com>
2019-05-19 20:41:35 +0200

    Update 1_AudioFile.py

--- InMoov/services/1_AudioFile.py
+++ InMoov/services/1_AudioFile.py
@@ -6,6 +6,19 @@
 # ##############################################################################
 # MRL SERVICE CALL
 # ##############################################################################
+#read current service part config based on file name
+ThisServicePart=RuningFolder+'config/service_'+os.path.basename(inspect.stack()[0][1]).replace('.py','')
+
+CheckFileExist(ThisServicePart)
+ThisServicePartConfig = ConfigParser.ConfigParser()
+ThisServicePartConfig.read(ThisServicePart+'.config')
+
+MyMusicPath=ThisServicePartConfig.get('AUDIO', 'MyMusicPath')
+
+#for noworky
+log.info("MyMusicPath : "+str(MyMusicPath))
+musicpath = MyMusicPath
+
 
 #startup sound
 AudioPlayer = Runtime.createAndStart("AudioPlayer", "AudioFile")
@@ -62,3 +75,38 @@ def AudioPlay(file):
 if StartupSound:AudioPlayer.playFileBlocking(RuningFolder+'/system/sounds/startupsound.mp3')
 python.subscribe(AudioPlayer.getName(),"publishAudioStart")
 python.subscribe(AudioPlayer.getName(),"publishAudioEnd")
+
+# play a song from your music directory
+def play():
+    global musiconoff
+    musiconoff = 1
+    #musicpath = (RuningFolder+"/system/sounds/")
+    files = os.listdir(musicpath)
+    song=random.choice(files)
+    sleep(3)
+    #i01.mouth.speakBlocking("playing song" + str(song))
+    AudioPlayer.playFile(musicpath + str(song) , False)
+    print("playing song:" + str(song))
+    sleep(1)
+    ear.startListening()
+    ear.setAutoListen(True)
+
+
+
+# pause method 
+def pause():
+    if musiconoff == 1:
+      AudioPlayer.pause()
+      print "set music on pause"
+
+# resume method 
+def resume():
+    if musiconoff == 0:
+      AudioPlayer.resume()
+      print "resuming music"
+        
+# stopped method is called when at the end of an audio file
+def stop():
+    if musiconoff == 1:
+        AudioPlayer.stop()
+        print "stop playing"
