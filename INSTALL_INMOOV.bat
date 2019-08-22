efc252dd221eeed68988f12551554f6f537bf9d4
Langevin Gael <hairygael@gmail.com>
2019-08-23 00:41:47 +0200

    Update INSTALL_INMOOV.bat

--- INSTALL_INMOOV.bat
+++ INSTALL_INMOOV.bat
@@ -16,34 +16,7 @@ echo ------------------------------------------------------
 del myrobotlab.log.1 > NUL
 mv myrobotlab.log myrobotlab.log.1
 
-echo "Done."
-echo ------------------------------------------------------
-echo UPDATE MRL INSTALLATION
-echo ------------------------------------------------------
 timeout 1 > NUL
-echo .
-if exist %cd%\InMoov\system\updated RMDIR /S /Q .myrobotlab
-if exist %cd%\InMoov\system\updated del %cd%\InMoov\system\updated
-move /y %cd%\myrobotlab-*.jar %cd%\myrobotlab.jar > NUL
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q haarcascades
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q hogcascades
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q lbpcascades
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q libraries
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q pythonModules
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q resource
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q tessdata
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q InMoov\chatbot\bots\de\aimlif
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q InMoov\chatbot\bots\es\aimlif
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q InMoov\chatbot\bots\en\aimlif
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q InMoov\chatbot\bots\fr\aimlif
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q InMoov\chatbot\bots\hi\aimlif
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q InMoov\chatbot\bots\nl\aimlif
-if exist %cd%\mrlNeedReinstall RMDIR /S /Q InMoov\chatbot\bots\ru\aimlif
-if exist %cd%\mrlNeedReinstall del ivychain.xml
-if exist %cd%\mrlNeedReinstall del myrobotlab.log
-if exist %cd%\mrlNeedReinstall del repo.json
-if exist %cd%\mrlNeedReinstall del %cd%\.myrobotlab\serviceData.json
-if exist %cd%\mrlNeedReinstall del mrlNeedReinstall
 COLOR 2F
 cls
 echo ------------------------------------------------------
@@ -54,7 +27,7 @@ echo          !!!            DO NOT CLOSE         !!!
 echo ------------------------------------------------------
 timeout 2 > NUL
 java -Dfile.encoding=UTF-8 -jar myrobotlab.jar --install RasPi InMoov VoiceRss WikiDataFetcher Polly ProgramAB AzureTranslator LocalSpeech IndianTts
-timeout 20 > NUL
+timeout 10 > NUL
 COLOR 3F
 cls
 echo ------------------------------------------------------
