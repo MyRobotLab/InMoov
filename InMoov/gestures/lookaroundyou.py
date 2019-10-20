ee6e66db602c2061f34411dd972ece14745a646c
Langevin Gael <hairygael@gmail.com>
2019-10-20 23:22:54 +0200

    Update lookaroundyou.py

--- InMoov/gestures/lookaroundyou.py
+++ InMoov/gestures/lookaroundyou.py
@@ -1,52 +1,34 @@
-def lookaroundyou(data):
-  i01.setHeadVelocity(36.0, 36.0, 19.0, 19.0, -1)
-  for y in range(0, 3):
-    if (data == "can i have your attention"):
-      i01.mouth.speak("ok you have my attention")
-      stopit()
-      x = (random.randint(1, 6))
-      if x == 1:
-        i01.head.neck.moveTo(90)
-        eyeslooking(data)
-      if x == 2:
-        i01.head.rothead.moveTo(80)
-        eyeslooking(data)
-      if x == 3:
-        headdown()
-        eyeslooking(data)
-      if x == 4:
-        headupp()
-        eyeslooking(data)
-      if x == 5:
-        headright()
-        eyeslooking(data)
-      if x == 6:
-        headleft()
-        eyeslooking(data)
-      sleep(1)
+def lookaroundyou():
+  i01.setHeadVelocity(80, 80, 50, 50, -1)
+  i01.mouth.speak("ok you have my attention")
+  for x in range(0, 1):
+    x = (random.randint(1, 6))
+    if x == 1:
+      i01.head.neck.moveTo(90)
+      eyeslooking()
+    if x == 2:
+      i01.head.rothead.moveTo(80)
+      eyeslooking()
+    if x == 3:
+      headdown()
+      eyeslooking()
+    if x == 4:
+      headupp()
+      eyeslooking()
+    if x == 5:
+      headright()
+      eyeslooking()
+    if x == 6:
+      headleft()
+      eyeslooking()
+    sleep(1)
     x = (random.randint(1, 4))
     if x == 1:
       i01.mouth.speak("looking nice")
     if x == 2:
       i01.mouth.speak("i like it here")
     if x == 3:
-      i01.mouth.speak("time just flies away")
+      i01.mouth.speak("can I help you")
     if x == 4:
-      i01.mouth.speak("ok let's do something")
-      sleep(2)
-      x = (random.randint(1, 4))
-      if x == 1:
-        comehere()
-      if x == 2:
-        perfect()
-        sleep(2)
-        rest()
-        sleep(1)
-        relax()
-      if x == 3:
-        rest()
-      if x == 4:
-        fingerleft()
-        sleep(3)
-        relax()
-
+      i01.mouth.speak("what would you like to do")
+    relax()
