1613a2aae833fbad6b3e82a8b9665cb3ae0a8f29
Langevin Gael <hairygael@gmail.com>
2021-05-26 18:48:58 +0200

    add ultrasonic right left

--- InMoov/gestures/ultraSonic.py
+++ InMoov/gestures/ultraSonic.py
@@ -1,2 +1,2 @@
 def ultraSonic(returnText):
-  i01.speakBlocking(unicode(returnText,'utf-8')+str(i01.getUltrasonicSensorDistance()).replace(".0", ""))
+  i01.speakBlocking(unicode(returnText,'utf-8')+str(i01.getUltrasonicRightDistance()).replace(".0", "")+str(i01.getUltrasonicLeftDistance()).replace(".0", ""))
