1f3c02fffd8d829b55d72f204a0bdf96db3664c0
Langevin Gael <hairygael@gmail.com>
2021-06-23 17:19:31 +0200

    replace velocity to speed

--- InMoov/minimal/minimalHead.py
+++ InMoov/minimal/minimalHead.py
@@ -4,42 +4,42 @@
 
 def lookleftside():
   i01.startedGesture()
-  i01.setHeadVelocity(60, 60)
+  i01.setHeadSpeed(60, 60)
   i01.moveHeadBlocking(85,160)
   i01.finishedGesture()
   
 def lookrightside():
   i01.startedGesture()
-  i01.setHeadVelocity(60, 60)
+  i01.setHeadSpeed(60, 60)
   i01.moveHeadBlocking(85,20)
   i01.finishedGesture()
 
 def lookinmiddle():
   i01.startedGesture()
-  i01.setHeadVelocity(60, 60)
+  i01.setHeadSpeed(60, 60)
   i01.moveHeadBlocking(85,90)
   i01.finishedGesture()
   
 def lookup():
   i01.startedGesture()
-  i01.setHeadVelocity(60, 60)
+  i01.setHeadSpeed(60, 60)
   i01.moveHeadBlocking(175,90)
   i01.finishedGesture()
 
 def lookdown():
   i01.startedGesture()
-  i01.setHeadVelocity(60, 60)
+  i01.setHeadSpeed(60, 60)
   i01.moveHeadBlocking(10,90) 
   i01.finishedGesture()  
   
 def tiltHeadLeftSide():
   i01.startedGesture()
-  i01.setHeadVelocity(60, 60, 60)
+  i01.setHeadSpeed(60, 60, 60)
   i01.moveHeadBlocking(85,90,0)
   i01.finishedGesture()
   
 def tiltHeadRightSide():
   i01.startedGesture()
-  i01.setHeadVelocity(60, 60, 60)
+  i01.setHeadSpeed(60, 60, 60)
   i01.moveHeadBlocking(85,90,180)
   i01.finishedGesture()
